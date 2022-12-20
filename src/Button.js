174680984fe10ceb6ea8f17a77ccbb81b744d306
lkj3085 <lkj3085@gmail.com>
2022-12-20 17:02:02 +0900

    '완성'

---
+++ src/Button.js
@@ -0,0 +1,36 @@
+import { StyleSheet, TouchableOpacity, Text } from "react-native";
+
+export default (props) => {
+  const { onPress, isSelected, text } = props;
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={[
+        styles.container,
+        isSelected ? styles.selectedButton : styles.notSelectedButton,
+      ]}>
+      <Text style={styles.buttonText}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#ffffff80",
+    borderWidth: 2,
+    borderRadius: 15,
+    paddingHorizontal: 10,
+    paddingVertical: 3,
+    marginHorizontal: 5,
+  },
+  selectedButton: {
+    borderColor: "white",
+  },
+  notSelectedButton: {
+    borderColor: "transparent",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+  },
+});
---
+++ src/use-cookie.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+const getRandomCookieKey = () => {
+  const cookieLen = 15;
+  const randomNum = Math.floor(Math.random() * cookieLen);
+  // console.log(Math.random());
+  // console.log(randomNum);
+
+  //randomNum = 0 ~ 14
+  //randomNum + 1 = 1~ 15
+  return `cookie_${randomNum + 1}`;
+};
+
+export const useCookie = () => {
+  const [cookieKey, setCookieKey] = useState("");
+
+  useEffect(() => {
+    const randomCookieKey = getRandomCookieKey();
+    setTimeout(() => {
+      setCookieKey(randomCookieKey);
+    }, 2000);
+  }, []);
+  return { cookieKey };
+};
